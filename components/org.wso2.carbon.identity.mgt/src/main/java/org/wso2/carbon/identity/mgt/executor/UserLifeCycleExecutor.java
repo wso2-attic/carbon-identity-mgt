d979565bbfbbc154736bf99a20a8740601eb6356
isuraD <isuradilhara@gmail.com>
2017-02-16 22:24:55 +0530

    Fixing typo and rename UserLifeCycle.xml file

--- components/org.wso2.carbon.identity.mgt/src/main/java/org/wso2/carbon/identity/mgt/executor/UserLifeCycleExecutor.java
+++ components/org.wso2.carbon.identity.mgt/src/main/java/org/wso2/carbon/identity/mgt/executor/UserLifeCycleExecutor.java
@@ -1,22 +1,20 @@
-/***********************************************************************************************************************
- * *
- * *   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * *
- * *   WSO2 Inc. licenses this file to you under the Apache License,
- * *   Version 2.0 (the "License"); you may not use this file except
- * *   in compliance with the License.
- * *   You may obtain a copy of the License at
- * *
- * *     http://www.apache.org/licenses/LICENSE-2.0
- * *
- * *  Unless required by applicable law or agreed to in writing,
- * *  software distributed under the License is distributed on an
- * *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * *  KIND, either express or implied.  See the License for the
- * *  specific language governing permissions and limitations
- * *  under the License.
- * *
- */
+/*
+*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 package org.wso2.carbon.identity.mgt.executor;
 
@@ -56,7 +54,5 @@ public class UserLifeCycleExecutor implements Executor {
      */
     @Override
     public void execute(Object resource, String currentState, String targetState) throws LifecycleException {
-        //
-        log.info(currentState);
     }
 }
