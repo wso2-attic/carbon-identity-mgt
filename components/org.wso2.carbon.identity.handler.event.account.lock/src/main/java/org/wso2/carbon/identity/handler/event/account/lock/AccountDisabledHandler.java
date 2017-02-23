e3a6167fa3e8a096e628ac9fcd806116d51e4e2f
Isura <isuradilhara@gmail.com>
2017-02-23 20:26:15 +0530

    Format code (#151)
    * Format code

--- components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/AccountDisabledHandler.java
+++ components/org.wso2.carbon.identity.handler.event.account.lock/src/main/java/org/wso2/carbon/identity/handler/event/account/lock/AccountDisabledHandler.java
@@ -14,7 +14,6 @@
  * limitations und
  */
 
-
 package org.wso2.carbon.identity.handler.event.account.lock;
 
 import org.slf4j.Logger;
@@ -36,7 +35,6 @@ import org.wso2.carbon.identity.mgt.IdentityStore;
 import org.wso2.carbon.identity.mgt.User;
 import org.wso2.carbon.identity.mgt.UserState;
 import org.wso2.carbon.identity.mgt.claim.Claim;
-import org.wso2.carbon.identity.mgt.constant.StoreConstants;
 import org.wso2.carbon.identity.mgt.exception.IdentityStoreException;
 import org.wso2.carbon.identity.mgt.exception.UserNotFoundException;
 import org.wso2.carbon.lcm.core.LifecycleOperationManager;
@@ -46,13 +44,14 @@ import org.wso2.carbon.lcm.sql.beans.LifecycleHistoryBean;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.wso2.carbon.identity.mgt.UserState.DISABLED;
+import static org.wso2.carbon.identity.mgt.constant.StoreConstants.IdentityStoreConstants;
+import static org.wso2.carbon.identity.mgt.constant.StoreConstants.IdentityStoreInterceptorConstants;
 import static org.wso2.carbon.kernel.utils.LambdaExceptionUtils.rethrowConsumer;
 import static org.wso2.carbon.kernel.utils.StringUtils.isNullOrEmpty;
-
-
 /**
  * Account Disable Handler class
  */
@@ -64,18 +63,18 @@ public class AccountDisabledHandler extends AbstractEventHandler {
     private static final String TEMPLATE_TYPE = "TEMPLATE_TYPE";
 
     private static List<String> events = Arrays.asList(
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PUT,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PATCH,
-            StoreConstants.IdentityStoreInterceptorConstants.POST_UPDATE_USER_CLAIMS_PATCH,
-            StoreConstants.IdentityStoreInterceptorConstants.POST_UPDATE_USER_CLAIMS_PUT,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PUT,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PATCH,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_DELETE_USER,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PUT,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PATCH,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PUT,
-            StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PATCH,
-            StoreConstants.IdentityStoreInterceptorConstants.POST_AUTHENTICATE);
+            IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PUT,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PATCH,
+            IdentityStoreInterceptorConstants.POST_UPDATE_USER_CLAIMS_PATCH,
+            IdentityStoreInterceptorConstants.POST_UPDATE_USER_CLAIMS_PUT,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PUT,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PATCH,
+            IdentityStoreInterceptorConstants.PRE_DELETE_USER,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PUT,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PATCH,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PUT,
+            IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PATCH,
+            IdentityStoreInterceptorConstants.POST_AUTHENTICATE);
     @Override
     public void handle(EventContext eventContext, Event event) throws EventException {
         if (!accountLockBean.isAccountInactiveEnabled()) {
@@ -85,74 +84,74 @@ public class AccountDisabledHandler extends AbstractEventHandler {
         IdentityStore identityStore = AccountServiceDataHolder.getInstance().getRealmService().getIdentityStore();
 
         try {
-            if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PUT
+            if (IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PUT
                     .equals(event.getEventName())) {
                 handlePreUpdateUserClaimsPUT(eventContext, event, identityStore);
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PATCH
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CLAIMS_PATCH
                     .equals(event.getEventName())) {
                 handlePreUpdateUserClaimsPATCH(eventContext, event, identityStore);
 
-            } else if (event.getEventName().equals(StoreConstants.IdentityStoreInterceptorConstants
+            } else if (event.getEventName().equals(IdentityStoreInterceptorConstants
                     .POST_UPDATE_USER_CLAIMS_PATCH) ||
-                    event.getEventName().equals(StoreConstants.IdentityStoreInterceptorConstants
+                    event.getEventName().equals(IdentityStoreInterceptorConstants
                             .POST_UPDATE_USER_CLAIMS_PUT)) {
                 //Post update user PUT and Post update user PATCH
                 handlePostUpdateUserClaims(eventContext, event);
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PUT
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PUT
                     .equals(event.getEventName())) {
                 handleDisableFromUniqueUserId(event, identityStore);
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PATCH
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_USER_CREDENTIALS_PATCH
                     .equals(event.getEventName())) {
                 handleDisableFromUniqueUserId(event, identityStore);
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_DELETE_USER
+            } else if (IdentityStoreInterceptorConstants.PRE_DELETE_USER
                     .equals(event.getEventName())) {
                 handleDisableFromUniqueUserId(event, identityStore);
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PUT
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PUT
                     .equals(event.getEventName())) {
                 handleDisableFromUniqueUserId(event, identityStore);
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PATCH
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_GROUPS_OF_USER_PATCH
                     .equals(event.getEventName())) {
                 handleDisableFromUniqueUserId(event, identityStore);
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PUT.equals(event
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PUT.equals(event
                     .getEventName())) {
                 List<String> uniqueUserIds = (List<String>) event.getEventProperties().get(
-                        StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID_LIST);
+                        IdentityStoreConstants.UNIQUE_USED_ID_LIST);
                 uniqueUserIds.forEach(rethrowConsumer(uniqueUserId -> {
                     User user = identityStore.getUser(uniqueUserId);
                     if (user != null && UserState.DISABLED.toString().equals(user.getState())) {
-                        throw new EventException("User account is disabled :" + user.getUniqueUserId());
+                        throw new EventException("User account is disabled: " + user.getUniqueUserId());
                     }
                 }));
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PATCH
+            } else if (IdentityStoreInterceptorConstants.PRE_UPDATE_USERS_OF_GROUP_PATCH
                     .equals(event.getEventName())) {
                 List<String> uniqueUserIdsToAdd = (List<String>) event.getEventProperties().get(
-                        StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID_LIST_TO_ADD);
+                        IdentityStoreConstants.UNIQUE_USED_ID_LIST_TO_ADD);
                 uniqueUserIdsToAdd.forEach(rethrowConsumer(uniqueUserId -> {
                     User user = identityStore.getUser(uniqueUserId);
                     if (user != null && UserState.DISABLED.toString().equals(user.getState())) {
-                        throw new EventException("User account is disabled :" + user.getUniqueUserId());
+                        throw new EventException("User account is disabled: " + user.getUniqueUserId());
                     }
                 }));
 
                 List<String> uniqueUserIdsToRemove = (List<String>) event.getEventProperties().get(
-                        StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID_LIST_TO_REMOVE);
+                        IdentityStoreConstants.UNIQUE_USED_ID_LIST_TO_REMOVE);
                 uniqueUserIdsToRemove.forEach(rethrowConsumer(uniqueUserId -> {
                     User user = identityStore.getUser(uniqueUserId);
                     if (user != null && UserState.DISABLED.toString().equals(user.getState())) {
-                        throw new EventException("User account is disabled :" + user.getUniqueUserId());
+                        throw new EventException("User account is disabled: " + user.getUniqueUserId());
                     }
                 }));
 
-            } else if (StoreConstants.IdentityStoreInterceptorConstants.POST_AUTHENTICATE
+            } else if (IdentityStoreInterceptorConstants.POST_AUTHENTICATE
                     .equals(event.getEventName())) {
                 AuthenticationContext authenticationContext = (AuthenticationContext) event.getEventProperties()
-                        .get(StoreConstants.IdentityStoreConstants.AUTHENTICATION_CONTEXT);
+                        .get(IdentityStoreConstants.AUTHENTICATION_CONTEXT);
 
                 if (authenticationContext == null) {
                     throw new EventException("No context found for authentication");
@@ -160,7 +159,7 @@ public class AccountDisabledHandler extends AbstractEventHandler {
 
                 if (authenticationContext.isAuthenticated()) {
                     if (UserState.DISABLED.toString().equals(authenticationContext.getUser().getState())) {
-                        throw new EventException("User account is disabled :" + authenticationContext.getUser()
+                        throw new EventException("User account is disabled: " + authenticationContext.getUser()
                                 .getUniqueUserId());
                     }
                 }
@@ -203,7 +202,7 @@ public class AccountDisabledHandler extends AbstractEventHandler {
             boolean oldAccountDisabledValue = (boolean) accountDisabledObject;
 
             String uniqueUserId = (String) event.getEventProperties()
-                    .get(StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID);
+                    .get(IdentityStoreConstants.UNIQUE_USED_ID);
 
             try {
                 boolean newAccountDisabledClaimValue = Boolean.parseBoolean(getUserClaimValue(uniqueUserId,
@@ -217,12 +216,10 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                     }
                 }
             } catch (IdentityStoreException | UserNotFoundException e) {
-                throw new EventException("Error while reading claims of user :" + uniqueUserId);
+                throw new EventException("Error while reading claims of user: " + uniqueUserId);
             }
-
         }
     }
-
 
     /**
      * Update user to disable state by admin
@@ -241,7 +238,6 @@ public class AccountDisabledHandler extends AbstractEventHandler {
             triggerNotification(uniqueUserId, AccountConstants.EMAIL_TEMPLATE_TYPE_ACC_DISABLED);
         }
     }
-
 
     /**
      * Update user to enabled state by admin
@@ -262,7 +258,6 @@ public class AccountDisabledHandler extends AbstractEventHandler {
         }
     }
 
-
     /**
      * This is used to handle pre update user claims PUT
      *
@@ -274,9 +269,9 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                                               IdentityStore identityStore) throws EventException,
             IdentityStoreException, UserNotFoundException {
         String uniqueUserId = (String) event.getEventProperties()
-                .get(StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID);
+                .get(IdentityStoreConstants.UNIQUE_USED_ID);
         List<Claim> claims = (List<Claim>) event.getEventProperties()
-                .get(StoreConstants.IdentityStoreConstants.CLAIM_LIST);
+                .get(IdentityStoreConstants.CLAIM_LIST);
 
         Optional<Claim> optional = claims.stream()
                 .filter(claim -> AccountConstants.ACCOUNT_DISABLED_CLAIM.equals(claim.getClaimUri()))
@@ -291,7 +286,7 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                 }
 
             } catch (IdentityStoreException | UserNotFoundException e) {
-                throw new EventException("Error while reading claims of user :" + uniqueUserId);
+                throw new EventException("Error while reading claims of user: " + uniqueUserId);
             }
         } else {
             handleDisableFromUniqueUserId(event, identityStore);
@@ -309,9 +304,9 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                                                 Event event, IdentityStore identityStore)
             throws EventException, IdentityStoreException, UserNotFoundException {
         String uniqueUserId = (String) event.getEventProperties()
-                .get(StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID);
+                .get(IdentityStoreConstants.UNIQUE_USED_ID);
         List<Claim> claimsToAdd = (List<Claim>) event.getEventProperties()
-                .get(StoreConstants.IdentityStoreConstants.CLAIM_LIST_TO_ADD);
+                .get(IdentityStoreConstants.CLAIM_LIST_TO_ADD);
 
         Optional<Claim> optional = claimsToAdd.stream()
                 .filter(claim -> AccountConstants.ACCOUNT_DISABLED_CLAIM.equals(claim.getClaimUri()))
@@ -326,7 +321,7 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                 }
 
             } catch (IdentityStoreException | UserNotFoundException e) {
-                throw new EventException("Error while reading claims of user :" + uniqueUserId);
+                throw new EventException("Error while reading claims of user: " + uniqueUserId);
             }
         } else {
             handleDisableFromUniqueUserId(event, identityStore);
@@ -345,15 +340,14 @@ public class AccountDisabledHandler extends AbstractEventHandler {
     private void handleDisableFromUniqueUserId(Event event, IdentityStore identityStore)
             throws IdentityStoreException, UserNotFoundException, EventException {
 
-        String userId = (String) event.getEventProperties().get(
-                StoreConstants.IdentityStoreConstants.UNIQUE_USED_ID);
+        String userId = (String) event.getEventProperties().get(IdentityStoreConstants.UNIQUE_USED_ID);
         User user = identityStore.getUser(userId);
         if (user != null && UserState.DISABLED.toString().equals(user.getState())) {
             if (log.isDebugEnabled()) {
-                log.debug("User account is disabled :" + user.getUniqueUserId());
+                log.debug("User account is disabled: " + user.getUniqueUserId());
             }
 
-            throw new EventException("User account is disabled :" + user.getUniqueUserId());
+            throw new EventException("User account is disabled: " + user.getUniqueUserId());
         }
     }
 
@@ -383,7 +377,6 @@ public class AccountDisabledHandler extends AbstractEventHandler {
         return null;
     }
 
-
     /**
      * This is used ot update user lifecycle states in Account Disabled handler
      *
@@ -405,6 +398,9 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                 List<LifecycleHistoryBean> lifecycleHistoryFromId = LifecycleOperationUtil
                         .getLifecycleHistoryFromId(domainUserId);
 
+                if (lifecycleHistoryFromId.size() == 0) {
+                    throw new EventException("Lifecycle history is disabled.");
+                }
                 String previousState = lifecycleHistoryFromId.get(lifecycleHistoryFromId.size() - 1).getPreviousState();
                 // if the user is disabled and then enabled, he should be in the unlocked state. CREATED state is not
                 // suitable
@@ -414,9 +410,8 @@ public class AccountDisabledHandler extends AbstractEventHandler {
                 LifecycleOperationManager.executeLifecycleEvent(previousState, domainUserId, domainUserId, null);
                 identityStore.setUserState(user.getUniqueUserId(), previousState);
             }
-
         } catch (LifecycleException | UserNotFoundException | IdentityStoreException e) {
-            throw new EventException("Error while executing lifecycle event for user :" + user
+            throw new EventException("Error while executing lifecycle event for user: " + user
                     .getUniqueUserId(), e);
         }
     }
@@ -439,11 +434,10 @@ public class AccountDisabledHandler extends AbstractEventHandler {
         return decodedUniqueEntityIdParts[1];
     }
 
-
     private void triggerNotification(String userUniqueId, String type)
             throws EventException {
         String eventName = EventConstants.Event.TRIGGER_NOTIFICATION;
-        HashMap<String, Object> properties = new HashMap<>();
+        Map<String, Object> properties = new HashMap<>();
         properties.put(EventConstants.EventProperty.USER_UNIQUE_ID, userUniqueId);
 
         properties.put(TEMPLATE_TYPE, type);
@@ -454,7 +448,7 @@ public class AccountDisabledHandler extends AbstractEventHandler {
             AccountServiceDataHolder.getInstance().getIdentityEventService().pushEvent(identityMgtEvent,
                     eventContext);
         } catch (IdentityException e) {
-            log.warn("Error while sending email : " + type + " for user :" + userUniqueId);
+            log.warn("Error while sending email: " + type + " for user: " + userUniqueId);
         }
     }
 
